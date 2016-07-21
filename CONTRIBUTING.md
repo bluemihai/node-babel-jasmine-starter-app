a0319a43d31485be4b703784c55352bc1cc7ec8d
Mihai <mihai.banulescu@gmail.com>
2016-07-21 06:26:47 -0700

    jasmine working

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,1 +1,7 @@
-Setting up your node environment
+Set up your dev environment
+
+```
+  [tab 1] npm run buildwatch
+  [tab 2] npm start
+  [tab 3] npm test
+```
--- src/index.js
+++ src/index.js
@@ -2,20 +2,9 @@
 
 const express = require('express')
 const app = express()
-const request = require('request')
 const path = require('path')
 
 app.set('port', (process.env.PORT || 3000))
-
-let headers = {'User-Agent': 'MB'}
-let uri = 'https://woodpecker-instagram-api.herokuapp.com/api/v1/locations'
-let options = { headers, uri }
-
-request(options, (error, response, body) => {
-  if (!error && response.statusCode == 200) {
-    console.log(body);
-  }
-})
 
 app.use(
   express.static(path.join(__dirname, '/public'))
